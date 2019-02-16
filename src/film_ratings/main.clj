c81481ead983299bcbd38009f45d4a7fb599e135
jahson <jjahson@gmail.com>
2019-02-16 14:39:39 +0300

    Migrate to duct 0.7.0

--- src/film_ratings/main.clj
+++ src/film_ratings/main.clj
@@ -1,12 +1,12 @@
 (ns film-ratings.main
   (:gen-class)
-  (:require [clojure.java.io :as io]
-            [duct.core :as duct]))
+  (:require [duct.core :as duct]))
 
 (duct/load-hierarchy)
 
 (defn -main [& args]
-  (let [keys (or (duct/parse-keys args) [:duct/daemon])]
-    (-> (duct/read-config (io/resource "film_ratings/config.edn"))
-        (duct/prep keys)
-        (duct/exec keys))))
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "film_ratings/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))))
